cc48f43602d8aa633c9aff6b0733256af91ace96
Roxedus <me@roxedus.dev>
2022-04-05 23:05:30 +0200

    Adjust to new paths

--- root/org/cron.sh
+++ root/org/cron.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
-if [[ -a /config/www/organizr/cron.php ]]; then
-  echo "Running cron job..."
+if [[ -e /config/www/organizr/cron.php ]]; then
   s6-setuidgid abc /usr/bin/php7 /config/www/organizr/cron.php
 else
   echo "No cron file to run..."
