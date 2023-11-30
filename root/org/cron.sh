0720eaf155a88251062cda244a8beaa91218a689
Roxedus <me@roxedus.dev>
2023-11-30 10:16:05 +0100

    Bump to Alpine 3.18

--- root/org/cron.sh
+++ root/org/cron.sh
@@ -2,7 +2,7 @@
 # shellcheck shell=bash
 
 if [[ -e /config/www/organizr/cron.php ]]; then
-  s6-setuidgid abc /usr/bin/php7 /config/www/organizr/cron.php
+  s6-setuidgid abc /usr/bin/php81 /config/www/organizr/cron.php
 else
   echo "No cron file to run..."
 fi
